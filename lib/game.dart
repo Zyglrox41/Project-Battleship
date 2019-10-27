f16e8c8f6a8681dd02c27bd201a869b94984b8f3
Thomas <zyglrox95@gmail.com>
2019-10-28 00:17:12 +1100

    Add marker support

--- lib/game.dart
+++ lib/game.dart
@@ -2,15 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
 //import 'package:geoflutterfire/geoflutterfire.dart';
-//import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:location/location.dart';
 
 class Game extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      body: GameMap(),
-    ));
+    return MaterialApp(title: "Battleships", home: Scaffold(body: GameMap()));
   }
 }
 
@@ -21,7 +19,10 @@ class GameMap extends StatefulWidget {
 class GameMapState extends State<GameMap> {
   Completer<GoogleMapController> _controller = Completer();
   GoogleMapController mapController;
-  Map<String, double> userLocation;
+  LatLng marker1 = new LatLng(-37.569792, 143.887614);
+  Location location = new Location();
+
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
 
   static CameraPosition _kUni = CameraPosition(
     target: LatLng(-37.569792, 143.887614),
@@ -46,31 +47,53 @@ class GameMapState extends State<GameMap> {
   }
 
   _addmarker() {
-    
-    
+    var markerIdVal = "1";
+    final MarkerId markerId = MarkerId(markerIdVal);
+
+    final Marker marker = Marker(
+        markerId: markerId,
+        position: marker1,
+        infoWindow: InfoWindow(title: markerIdVal, snippet: '*'),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+        visible: true,
+        onTap: () {});
+    setState(() {
+      // adding a new marker to map
+      markers[markerId] = marker;
+    });
+  }
+
+  _moveToUser() async {    
+    var pos = await location.getLocation();
+    mapController.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(
+        
+        zoom: 15.0,
+        )
+        )
+        );
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: AppBar(
-          title: Text("Battleships"),
-          centerTitle: true,
-          backgroundColor: Colors.green[600],
+    return new Stack(
+      children: <Widget>[
+        GoogleMap(
+          onMapCreated: _onMapCreated,
+          mapType: MapType.hybrid,
+          myLocationButtonEnabled: true,
+          initialCameraPosition: _kUni,
+          markers: Set<Marker>.of(markers.values),
         ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: 500,
-                child: GoogleMap(
-                    mapType: MapType.hybrid,
-                    myLocationButtonEnabled: true,
-                    initialCameraPosition: _kUni,
-                    onMapCreated: _onMapCreated),
-              ),
-            ],
-          ),
-        ));
+        Positioned(
+          bottom: 30,
+          right: 10,
+          child: FloatingActionButton(
+              onPressed: _addmarker,              
+              child: Icon(Icons.filter_tilt_shift,
+                  color: Colors.white, size: 50.0)),
+        )
+      ],
+    );
   }
 }
